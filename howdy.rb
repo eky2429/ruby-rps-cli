f87dac10fbc0cdd6092d618e5ffd0bdf7f31a8e8
Ethan Ky <132713901+eky2429@users.noreply.github.com>
2024-02-25 23:37:54 +0000

    Modifies methods to make game more polished

--- howdy.rb
+++
@@ -1,9 +0,0 @@
-pp "Hello, world!"
-
-pp "What's your name?"
-
-their_name = gets.chomp
-
-puts "Hello, " + their_name + "!"
-require "active_support/all"
-require "./goodbye.rb"
--- rps.rb
+++ rps.rb
@@ -1,84 +1,119 @@
 class Game
 
   private
-  attr_accessor :move_one
-  attr_accessor :move_two
-  attr_accessor :total_games
-  attr_accessor :num_player_one_wins
-  attr_accessor :num_player_two_wins
-  attr_accessor :num_ties
-
-  def greetUser()
-    puts "Hello, user(s). We are going to Play Rock Paper Scissors!"
-  end
-
-  def showScores()
-    puts "Total games: #{@total_games}"
-    puts "Total player one wins: #{@num_player_one_wins}"
-    puts "Total player two wins: #{@num_player_two_wins}"
-    puts "Total ties: #{@num_ties}"
-  end
-
-  def verifyMove()
-    if @moveOne == @moveTwo
-      puts "You both threw the same move! You guys tied!"
-      num_ties += 1
-    elsif (@moveOne == "rock" && @moveTwo == "scissors") || (@moveOne == "paper" && @moveTwo == "rock") || (@moveOne == "scissors" && @moveTwo == "paper")
-      puts "Player one threw #{@moveOne}. Player two threw #{@moveTwo}. Player one wins!"
-      num_player_one_wins += 1
-    else
-      puts "Player one threw #{@moveOne}. Player two threw #{@moveTwo}. Player two wins!"
-      num_player_two_wins += 1
-    end
-    total_games += 1
-    showScores()
-  end
-
-  def inputMoves()
-    puts "Player one, type in your move: (Rock, Paper, or Scissors)"
-    @move_one =gets.chomp()
-    puts "Now player two, type in your move: (Rock, Paper, or Scissors)"
-    @move_two = gets.chomp()
-    verifyMove()
-  end
+  attr_accessor :player_move
+  attr_accessor :scores
 
   public
   def initialize()
-    @move_one = ""
-    @moveTwo = ""
-    @total_games = 0
-    @num_player_one_wins = 0
-    @num_player_two_wins = 0
-    @num_ties = 0
+    @player_move = ""
+    
+    # About @scores:
+    #   1st element - total games played
+    #   2nd element - games player won
+    #   3rd element - games CPU won
+    #   4th element - tied games
+    @scores = [0, 0, 0, 0] 
 
-    greetUser()
-    inputMoves()
+    puts "Hello, user(s). We are going to Play Rock Paper Scissors!" #Greet user
+    inputMove() #Ask for input
   end
 
-  def move_one=(value)
-    @move_one = value
+  def printStats()
+    puts "Total games: #{@scores[0]}"
+    puts "Total player one wins: #{@scores[1]}"
+    puts "Total player two wins: #{@scores[2]}"
+    puts "Total ties: #{@scores[3]}"
+    if @scores[2] == 0
+      puts "Your win to lose ratio (times player won / times CPU won): 0"
+    else 
+      puts "Your win to lose ratio (times player won / times CPU won): #{@scores[1] / @scores[2]}"
+    end
+    puts "Your recent move: #{@player_move}"
   end
 
-  def move_two=(value)
-    @move_two = value
+  #Displays instructions
+  def printInstructions()
+    puts "Rock Paper Scissors is a fun game to enjoy!"
+    puts "Normally, this game is played by two people, but"
+    puts "in this one you play against a CPU / Robot."
+
+    puts "\n How cool is that? :)\n"
+
+    puts "Anyway, to play a move, you have to type in either 'rock', 'paper', or 'scissors' to play."
+    puts "Also, avoid adding extra spaces or unecessary characters to your input (The program won't like that)"
+    puts "When you play a move, the CPU will also play a move. (rock, paper, or scissors)\n"
+
+    puts "There are many ways to win the game."
+    puts "For example, you play rock, and the CPU plays scissors"
+    puts "or you play paper, and the CPU plays rock"
+    puts "and possibly you play scissors, and the CPU plays paper"
+
+    puts "\n in those cases, you would win. However, if the CPU plays rock"
+    puts "and you play scissors, or the CPU plays paper, and you play rock,"
+    puts "or the CPU play scissors, and you play paper, then you lose. :("
+
+    puts "\n but it's okay if you lose becauyse you can play again and again, and"
+    puts "you can try to beat the CPU more times than it did. :) *thumbs-up*"
+    puts "\n But if you both play the same move, then neither one of you win; it is a tie"
+
+    puts "\nDon't worry, I will keep track of your status for the duration of this program running."
+    puts "If you want to check your status, type 'stats' to see your progress in the game."
+
+    puts "\nIf you want to stop playing or close this program, type 'x'"
+    puts "And I'll wait for you again. ;)"
+
+    puts "\n I think that's all I got. So, you should be good to go. Alright, let's start!"
   end
 
-  def total_games=(value)
-    @total_games = value
+  def playGame()
+    computer_move = ['rock', 'paper', 'scissors'].sample()
+    scores[0] += 1
+
+    puts "You played #{@player_move}"
+    puts "The CPU played #{computer_move}"
+
+    if computer_move == @player_move
+      scores[3] += 1
+      puts "You are both tied!"
+    elsif (@player_move == 'rock' && computer_move == 'scissors') || (@player_move == 'paper' && computer_move == 'rock') || (@player_move == 'scissors' && computer_move == 'paper')
+      scores[1] += 1
+      puts "You won!"
+    else
+      scores[2] += 1
+      puts "You didn't win!"
+    end
+    
+    puts "Let's play again!"
   end
 
-  def num_player_one_wins=(value)
-    @num_player_one_wins = value
+  def verifyMove()
+    @player_move = @player_move.downcase
+    if @player_move == "rock" || @player_move == "paper" || @player_move == "scissors"
+      #Play game is user inputs move
+      playGame()
+    elsif @player_move == "help"
+      #Show instructions if user needs help
+      printInstructions()
+    elsif @player_move == "stats"
+      #Show states if user requests it
+      printStats()
+    elsif @player_move == "x"
+      #Show states if user requests it
+      puts "Aw, I'm sad to see you go :("
+      puts "But I'm sure I'll see you later! *waves good-bye*"
+      return ""
+    else
+      #Otherwise, tell uer his/her input is invalid
+      puts "Invalid move. Try again."
+    end
+    inputMove()
   end
 
-  def num_player_two_wins=(value)
-    @num_player_two_wins = value
+  def inputMove()
+    puts "Player, type in your move: (type 'help' you need help)" #Asks player for input
+    @player_move = gets.chomp() #Gets input
+    verifyMove() #Verifies input
   end
 
-  def num_ties=(value)
-    @num_ties = value
-  end
 end
-
-
-game = Game.new()
